e09c872142053f405c9c7f0f7e07f23bb7da3b4a
Paolo Vernazza <p.vernazza@pillohealth.com>
2019-02-25 11:43:24 +0100

    Replace deprecated HttpClient with OkHttp

--- ClientLibrary/lib/src/main/java/com/microsoft/projectoxford/face/rest/WebServiceRequest.java
+++ ClientLibrary/lib/src/main/java/com/microsoft/projectoxford/face/rest/WebServiceRequest.java
@@ -36,26 +36,17 @@ import com.google.gson.Gson;
 import com.microsoft.projectoxford.face.common.RequestMethod;
 import com.microsoft.projectoxford.face.common.ServiceError;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class WebServiceRequest {
     private static final String HEADER_KEY = "ocp-apim-subscription-key";
@@ -64,7 +55,7 @@ public class WebServiceRequest {
     private static final String OCTET_STREAM = "octet-stream";
     private static final String DATA = "data";
 
-    private HttpClient mClient = new DefaultHttpClient();
+    private OkHttpClient mClient = new OkHttpClient();
     private String mSubscriptionKey;
     private Gson mGson = new Gson();
 
@@ -96,15 +87,18 @@ public class WebServiceRequest {
     }
 
     private Object get(String url) throws ClientException, IOException {
-        HttpGet request = new HttpGet(url);
-        request.setHeader(HEADER_KEY, mSubscriptionKey);
 
-        HttpResponse response = this.mClient.execute(request);
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode == HttpStatus.SC_OK) {
-            return readInput(response.getEntity().getContent());
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .header(HEADER_KEY, mSubscriptionKey)
+                .build();
+
+        Response response = this.mClient.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return readInput(response);
         } else {
-            String json = readInput(response.getEntity().getContent());
+            String json = readInput(response);
             if (json != null) {
                 ServiceError error = mGson.fromJson(json, ServiceError.class);
                 if (error != null) {
@@ -112,25 +106,27 @@ public class WebServiceRequest {
                 }
             }
 
-            throw new ClientException("Error executing GET request!", statusCode);
+            throw new ClientException("Error executing GET request!", response.code());
         }
     }
 
 
     private Object patch(String url, Map<String, Object> data, String contentType) throws ClientException, IOException {
-        HttpPatch request = new HttpPatch(url);
-        request.setHeader(HEADER_KEY, mSubscriptionKey);
-        String json = mGson.toJson(data);
-        StringEntity entity = new StringEntity(json, "utf-8");
-        request.setEntity(entity);
-        request.setHeader(CONTENT_TYPE, APPLICATION_JSON);
-        HttpResponse response = mClient.execute(request);
 
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode == HttpStatus.SC_OK) {
-            return readInput(response.getEntity().getContent());
+        String json = mGson.toJson(data);
+
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .header(HEADER_KEY, mSubscriptionKey)
+                .patch(RequestBody.create(MediaType.get(APPLICATION_JSON), json))
+                .header(CONTENT_TYPE, APPLICATION_JSON);
+
+        Response response = this.mClient.newCall(builder.build()).execute();
+
+        if (response.isSuccessful()) {
+            return readInput(response);
         } else {
-            json = readInput(response.getEntity().getContent());
+            json = readInput(response);
             if (json != null) {
                 ServiceError error = mGson.fromJson(json, ServiceError.class);
                 if (error != null) {
@@ -138,40 +134,42 @@ public class WebServiceRequest {
                 }
             }
 
-            throw new ClientException("Error executing Patch request!", statusCode);
+            throw new ClientException("Error executing Patch request!", response.code());
         }
     }
 
 
     private Object post(String url, Map<String, Object> data, String contentType) throws ClientException, IOException {
-        HttpPost request = new HttpPost(url);
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .addHeader(HEADER_KEY, this.mSubscriptionKey);
+
         boolean isStream = false;
 
         if (contentType != null && !(contentType.length() == 0)) {
-            request.setHeader(CONTENT_TYPE, contentType);
+            builder.header(CONTENT_TYPE, contentType);
+
             if (contentType.toLowerCase(Locale.ENGLISH).contains(OCTET_STREAM)) {
                 isStream = true;
             }
         } else {
-            request.setHeader(CONTENT_TYPE, APPLICATION_JSON);
+            builder.addHeader(CONTENT_TYPE, APPLICATION_JSON);
         }
-
-        request.setHeader(HEADER_KEY, this.mSubscriptionKey);
 
         if (!isStream) {
             String json = mGson.toJson(data);
-            StringEntity entity = new StringEntity(json, "utf-8");
-            request.setEntity(entity);
+            builder.post(RequestBody.create(MediaType.get(APPLICATION_JSON), json));
         } else {
-            request.setEntity(new ByteArrayEntity((byte[]) data.get(DATA)));
+            builder.post(RequestBody.create(MediaType.get(contentType), (byte[]) data.get(DATA)));
         }
 
-        HttpResponse response = mClient.execute(request);
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode == HttpStatus.SC_OK || statusCode == HttpStatus.SC_ACCEPTED) {
-            return readInput(response.getEntity().getContent());
+        Response response = mClient.newCall(builder.build()).execute();
+
+
+        if (response.isSuccessful()) {
+            return readInput(response);
         } else {
-            String json = readInput(response.getEntity().getContent());
+            String json = readInput(response);
             if (json != null) {
                 ServiceError error = mGson.fromJson(json, ServiceError.class);
                 if (error != null) {
@@ -179,25 +177,26 @@ public class WebServiceRequest {
                 }
             }
 
-            throw new ClientException("Error executing POST request!", statusCode);
+            throw new ClientException("Error executing POST request!", response.code());
         }
     }
 
     private Object put(String url, Map<String, Object> data) throws ClientException, IOException {
-        HttpPut request = new HttpPut(url);
 
-        request.setHeader(HEADER_KEY, mSubscriptionKey);
         String json = mGson.toJson(data);
-        StringEntity entity = new StringEntity(json, "utf-8");
-        request.setEntity(entity);
-        request.setHeader(CONTENT_TYPE, APPLICATION_JSON);
-        HttpResponse response = mClient.execute(request);
 
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode == HttpStatus.SC_OK) {
-            return readInput(response.getEntity().getContent());
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .header(HEADER_KEY, mSubscriptionKey)
+                .header(CONTENT_TYPE, APPLICATION_JSON)
+                .put(RequestBody.create(MediaType.get(APPLICATION_JSON), json));
+
+        Response response = mClient.newCall(builder.build()).execute();
+
+         if (response.isSuccessful()) {
+            return readInput(response);
         } else {
-            json = readInput(response.getEntity().getContent());
+            json = readInput(response);
             if (json != null) {
                 ServiceError error = mGson.fromJson(json, ServiceError.class);
                 if (error != null) {
@@ -205,30 +204,28 @@ public class WebServiceRequest {
                 }
             }
 
-            throw new ClientException("Error executing PUT request!", statusCode);
+            throw new ClientException("Error executing PUT request!", response.code());
         }
     }
 
 
     private Object delete(String url, Map<String, Object> data) throws ClientException, IOException {
-        HttpResponse response;
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .header(HEADER_KEY, mSubscriptionKey);
+
         if (data == null || data.isEmpty()) {
-            HttpDelete request = new HttpDelete(url);
-            request.setHeader(HEADER_KEY, mSubscriptionKey);
-            response = mClient.execute(request);
+            builder.delete();
         } else {
-            HttpDeleteWithBody request = new HttpDeleteWithBody(url);
             String json = mGson.toJson(data);
-            StringEntity entity = new StringEntity(json, "utf-8");
-            request.setEntity(entity);
-            request.setHeader(CONTENT_TYPE, APPLICATION_JSON);
-            request.setHeader(HEADER_KEY, mSubscriptionKey);
-            response = mClient.execute(request);
+            builder.delete(RequestBody.create(MediaType.get(APPLICATION_JSON), json));
+            builder.header(CONTENT_TYPE, APPLICATION_JSON);
         }
 
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode != HttpStatus.SC_OK) {
-            String json = readInput(response.getEntity().getContent());
+        Response response = mClient.newCall(builder.build()).execute();
+
+        if (!response.isSuccessful()) {
+            String json = readInput(response);
             if (json != null) {
                 ServiceError error = mGson.fromJson(json, ServiceError.class);
                 if (error != null) {
@@ -236,10 +233,10 @@ public class WebServiceRequest {
                 }
             }
 
-            throw new ClientException("Error executing DELETE request!", statusCode);
+            throw new ClientException("Error executing DELETE request!", response.code());
         }
 
-        return readInput(response.getEntity().getContent());
+        return readInput(response);
     }
 
 
@@ -267,14 +264,10 @@ public class WebServiceRequest {
         return url.toString();
     }
 
-    private String readInput(InputStream is) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        StringBuffer json = new StringBuffer();
-        String line;
-        while ((line = br.readLine()) != null) {
-            json.append(line);
-        }
+    private String readInput(Response response) throws IOException {
+        if (response.body() == null)
+            return null;
 
-        return json.toString();
+        return response.body().string();
     }
 }
